074b1c9719d553c891a95cbcb508f1958b344364
Farhan Khan <121663493+COBRA-404-CYBER@users.noreply.github.com>
2023-02-10 17:10:56 +0600

    Add files via upload

---
+++ Cobra_stp.py
@@ -0,0 +1,215 @@
+import os
+import time
+import sys
+import random
+import requests
+#import your dady farhan :)
+def basic():
+  print("\n\nINSTALLING START IN 3 SEC...\n\n")
+  time.sleep(3)
+  os.system("pkg update")
+  os.system("pkg upgrade")
+  os.system("pkg install python -y")
+  os.system("pkg install python2 -y")
+  os.system("pkg install python3")
+  os.system("apt install tmux")
+  os.system("pip install requests")
+  os.system("pip2 install requests")
+  os.system("pip install mechanize")
+  os.system("pip2 install mechanize")
+  os.system("pip install bs4")
+  os.system("pip2 install bs4")
+  os.system("pip install rich")
+  os.system("pkg install ruby -y")
+  os.system("gem install lolcat")
+  os.system("pip install php")
+  os.system("pip2 install php")
+  os.system("pip install lolcat")
+  os.system("pip install futures")
+  os.system("pip2 install futures")
+  os.system("pkg install nano")
+  os.system("clear")
+  print("""\033[1;32m
+ _______  _______ _________          _______ 
+(  ____ \(  ____ \\__   __/|\     /|(  ____ )
+| (    \/| (    \/   ) (   | )   ( || (    )|
+| (_____ | (__       | |   | |   | || (____)|
+(_____  )|  __)      | |   | |   | ||  _____)
+      ) || (         | |   | |   | || (      
+/\____) || (____/\   | |   | (___) || )      
+\_______)(_______/   )_(   (_______)|/       
+                                                                                 
+  ____   ___  _   _ _____ 
+ |  _ \ / _ \| \ | | ____|
+ | | | | | | |  \| |  _|  
+ | |_| | |_| | |\  | |___ 
+ |____/ \___/|_| \_|_____|
+                          
+
+
+TNX FOR USING OUR TOOLS..
+
+
+
+""")
+
+def full():
+  print("\n\nSETUP START IN 3 SEC...\n\n")
+  time.sleep(3)
+  os.system("pkg update")
+  os.system("pkg upgrade")
+  os.system("pkg install python")
+  os.system("pkg install python2")
+  os.system("pkg install python3")
+  os.system("pkg install wget")
+  os.system("pkg install python-pip")
+  os.system("pkg install fish")
+  os.system("pkg install ruby")
+  os.system("pkg install help")
+  os.system("pkg install dnsutils  ")
+  os.system("pkg install php")
+  os.system("pkg install perl")
+  os.system("pkg install lua")
+  os.system("pkg install parallel")
+  os.system("pkg install nmap")
+  os.system("pkg install bash")
+  os.system("pkg install clang")
+  os.system("pkg install nano")
+  os.system("pkg install w3m")
+  os.system("pkg install hydra")
+  os.system("pkg install figlet")
+  os.system("pkg install cowsay")
+  os.system("pkg install curl")
+  os.system("pkg install tar")
+  os.system("pkg install zip")
+  os.system("pkg install unzip")
+  os.system("pkg install tor")
+  os.system("pkg install net-tools")
+  os.system("pkg install sudo")
+  os.system("pkg install wireshark")
+  os.system("apt install tmux")
+  os.system("pkg install crunch")
+  os.system("pkg install wgetrc")
+  os.system("pkg install wgetrc")
+  os.system("pkg install wcalc")
+  os.system("pkg install openssl")
+  os.system("pkg install openssl-tool")
+  os.system("pkg install bmon")
+  os.system("pkg install vpn")
+  os.system("pkg install unrar")
+  os.system("pkg install toilet")
+  os.system("pkg install proot")
+  os.system("pkg install net-tools")
+  os.system("pkg install vim")
+  os.system("pkg install vim-python")
+  os.system("pkg install ired")
+  os.system("pkg install goaccess")
+  os.system("pkg install golang")
+  os.system("pkg install kibi")
+  os.system("pkg install tsu")
+  os.system("pkg install mtools")
+  os.system("pkg install file")
+  os.system("pkg install vis")
+  os.system("pkg install pass")
+  os.system("pkg install pick")
+  os.system("pkg install chroot")
+  os.system("pkg install macchanger")
+  os.system("pkg install ninja")
+  os.system("pkg install elixir")
+  os.system("pkg install swift")
+  os.system("pkg install xmlstarlet")
+  os.system("pkg install fakeroot")
+  os.system("pkg install texinfo")
+  os.system("pkg install netcat")
+  os.system("pkg install wren")
+  os.system("pkg install gatling")
+  os.system("pkg install cvs")
+  os.system("pkg install ffmpeg")
+  os.system("pkg install screen")
+  os.system("pkg install neofetch")
+  os.system("pkg install mariadb")
+  os.system("pkg install picolisp")
+  os.system("pkg install cmatrix")
+  os.system("pkg install dropbear")
+  os.system("pkg install openssh")
+  os.system("pkg install python-pip")
+  os.system("pip2 install wget")
+  os.system("pip install bs4")
+  os.system("pip2 install bs4")
+  os.system("pip install requests")
+  os.system("pip2 install requests")
+  os.system("pip install mechanize")
+  os.system("pip2 install mechanize")
+  os.system("pip install php")
+  os.system("pip2 install php")
+  os.system("gem install lolcat")
+  os.system("clear")
+  print("""\033[1;32m
+	 _______  _______ _________          _______ 
+(  ____ \(  ____ \\__   __/|\     /|(  ____ )
+| (    \/| (    \/   ) (   | )   ( || (    )|
+| (_____ | (__       | |   | |   | || (____)|
+(_____  )|  __)      | |   | |   | ||  _____)
+      ) || (         | |   | |   | || (      
+/\____) || (____/\   | |   | (___) || )      
+\_______)(_______/   )_(   (_______)|/       
+                                             
+ ____   ___  _   _ _____ 
+|  _ \ / _ \| \ | | ____|
+| | | | | | |  \| |  _|  
+| |_| | |_| | |\  | |___ 
+|____/ \___/|_| \_|_____|
+                                    
+TNX FOR USING MY TOOLS..
+	
+	
+""")
+
+os.system("git pull")
+os.system("clear")
+logo="""\033[1;32m
+   __________  ____  ____  ___       ________  ___
+  / ____/ __ \/ __ )/ __ \/   |     /_  __/  |/  /
+ / /   / / / / __  / /_/ / /| |______/ / / /|_/ /
+/ /___/ /_/ / /_/ / _, _/ ___ |_____/ / / /  / /
+\____/\____/_____/_/ |_/_/  |_|    /_/ /_/  /_/
+                                         
+\033[1;33m╔═════════════════════════════════════════════╗
+║ \033[1;37m [\033[1;32m+\033[1;37m] AUTHOR   : \033[1;32mCOBRA                       \033[1;33m║
+║ \033[1;37m [\033[1;32m+\033[1;37m] GROUP    : \033[1;32mCOBRA 404 CYBER WORLD       \033[1;33m║
+║ \033[1;37m [\033[1;32m+\033[1;37m] PAGE     :\033[1;32m @Cobra.404.Cyber            \033[1;33m║
+║ \033[1;37m [\033[1;32m+\033[1;37m] WHATSAPP :\033[1;32m +8801838847447              \033[1;33m║
+║ \033[1;37m [\033[1;32m+\033[1;37m] TEAM     : \033[1;32mCOBRA 404 CYBER             \033[1;33m║
+║ \033[1;37m [\033[1;32m+\033[1;37m] CREATOR  : \033[1;32mFARHAN KHAN                \033[1;33m ║
+║ \033[1;37m [\033[1;32m+\033[1;37m] VERSION  : \033[1;32m0.01                    \033[1;33m    ║
+║ \033[1;37m [\033[1;32m+\033[1;37m] TOOL TYPE: \033[1;32mFREE               \033[1;33m         ║
+\033[1;33m╚═════════════════════════════════════════════╝
+×××××××××××××××××××××××××××××××××××××××××××××××
+\033[1;37m[\033[1;32m01\033[1;37m] \033[1;32mTERMUX BASIC SETUP {BEST}
+\033[1;37m[\033[1;32m02\033[1;37m]\033[1;32m TERMUX FULL SETUP
+\033[1;37m[\033[1;32m03\033[1;37m]\033[1;32m FOLLOW OUR PAGE FOR MORE TOOLS 
+\033[1;37m[\033[1;32m04\033[1;37m]\033[1;32m CONTACT ME ON FACEBOOK 
+\033[1;33m×××××××××××××××××××××××××××××××××××××××××××××××"""
+for i in logo:
+  sys.stdout.write(i)
+  sys.stdout.flush()
+  time.sleep(0.001)
+mtmt=input("\n\033[1;37m[\033[1;32m+\033[1;37m] YOUR CHOICE : ")
+mtmt=mtmt.replace(" ","")
+if mtmt=="1" or mtmt =="01":
+	basic()
+elif mtmt=="2" or mtmt=="02":
+  full()
+elif mtmt=="3" or mtmt=="03":
+  os.system("xdg-open https://www.facebook.com/Cobra.404.Cyber/")
+elif mtmt=="4" or mtmt=="04":
+  os.system("xdg-open https://www.facebook.com/virtua.jhonny.sins?mibextid=ZbWKwL")
+else:
+  print("\n\n[\033[1;31m!\033[1;37m]\033[1;31m WRONG OPTION ENTERED \n\n")
+  sys.exit()
+
+
+
+#open script tool if you copy then give me credits..
+
+
