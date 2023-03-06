428d24257416da6aabbb8e60eb8e940f097062b2
Farhan Khan <121663493+COBRA-404-CYBER@users.noreply.github.com>
2023-03-06 13:13:41 +0600

    Add files via upload

---
+++ COBRA-PKG.py
@@ -0,0 +1,14 @@
+import os
+import time
+os.system("clear")
+print("\n\033[38;5;195m[\033[38;5;46m+\033[38;5;195m]\033[38;5;46m CHECKING UPDATE 💚")
+time.sleep(2)
+os.system("git pull")
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/groups/1354738058401296/?ref=share&mibextid=NSMWB")
+
+import setup
+
+
+
